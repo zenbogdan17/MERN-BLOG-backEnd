84805da39d968714941a8a9e13c8ee36cd7a6cd7
Zen_bogdan <bogdanzinkiwskuy2@gmail.com>
2024-03-04 09:43:39 +0200

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use('/', routes);
 
 const port = process.env.PORT || 4444;
